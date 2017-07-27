df823316ea2475ae559b9fd17d8ffb2623996ade
Joshua Swiss <swissjoshua@gmail.com>
2017-07-27 11:56:12 +0300

    class dummy data mocked up and loading

--- data/load-sample-data.js
+++ data/load-sample-data.js
@@ -1,32 +1,37 @@
-require('dotenv').config({ path: __dirname + '/../variables.env' });
-const fs = require('fs');
+require("dotenv").config({ path: __dirname + "/../variables.env" });
+const fs = require("fs");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 
 // import all of our models - they need to be imported only once
-const School = require('../models/School');
+const School = require("../models/School");
 // const User = require('../models/User');
 // const Student = require('../models/Student');
 // const Teacher = require('../models/Teacher');
-// const Class = require('../models/Class');
+const Class = require("../models/Class");
 
-
-const schools = JSON.parse(fs.readFileSync(__dirname + '/school-data.json', 'utf-8'));
+const schools = JSON.parse(
+  fs.readFileSync(__dirname + "/school-data.json", "utf-8")
+);
 // const students = JSON.parse(fs.readFileSync(__dirname + '/student-data.json', 'utf-8'));
 // const users = JSON.parse(fs.readFileSync(__dirname + '/user-data.json', 'utf-8'));
-// const classes = JSON.parse(fs.readFileSync(__dirname + '/class-data.json', 'utf-8'));
+const classes = JSON.parse(
+  fs.readFileSync(__dirname + "/class-data.json", "utf-8")
+);
 // const teachers = JSON.parse(fs.readFileSync(__dirname + '/teacher-data.json', 'utf-8'));
 
 async function deleteData() {
-  console.log('😢😢 Goodbye Data...');
+  console.log("😢😢 Goodbye Data...");
   await School.remove();
   // await User.remove();
   // await Student.remove();
   // await Teacher.remove();
-  // await Class.remove();
-  console.log('Data Deleted. To load sample data, run\n\n\t npm run sample\n\n');
+  await Class.remove();
+  console.log(
+    "Data Deleted. To load sample data, run\n\n\t npm run sample\n\n"
+  );
   process.exit();
 }
 
@@ -36,16 +41,18 @@ async function loadData() {
     // await User.insertMany(users);
     // await Student.insertMany(students);
     // await Teacher.insertMany(teachers);
-    // await Class.insertMany(classes);
-    console.log('👍👍👍👍👍👍👍👍 Done!');
+    await Class.insertMany(classes);
+    console.log("👍👍👍👍👍👍👍👍 Done!");
     process.exit();
-  } catch(e) {
-    console.log('\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n');
+  } catch (e) {
+    console.log(
+      "\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n"
+    );
     console.log(e);
     process.exit();
   }
 }
-if (process.argv.includes('--delete')) {
+if (process.argv.includes("--delete")) {
   deleteData();
 } else {
   loadData();
--- models/Class.js
+++ models/Class.js
@@ -1,34 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 // allows us to make url-friendly names
-const slug = require('slugs');
-const timestamps = require('mongoose-timestamp');
+const slug = require("slugs");
+const timestamps = require("mongoose-timestamp");
 
 const classSchema = new mongoose.Schema({
-  code: {
+  level: {
     type: String,
     trim: true,
-    enum: [],
+    enum: [
+      "p1",
+      "p2",
+      "p3",
+      "p4",
+      "p5",
+      "p6",
+      "p7",
+      "p8",
+      "s1",
+      "s2",
+      "s3",
+      "s4"
+    ]
+  },
+  code: {
+    type: String,
+    trim: true
+    // TODO: figure out a way to query the school if there are other classes with same level...
   },
   school: {
     type: mongoose.Schema.ObjectId,
-    ref: 'School',
-    required: 'Every class must have a school!',
-  },
+    ref: "School",
+    required: "Every class must have a school!"
+  }
 });
 
 classSchema.plugin(timestamps);
 
 // Pre-save hook in MongoDB to auto create a slug
-classSchema.pre('save', async function (next) {
-  if (!this.isModified('name')) {
+classSchema.pre("save", async function(next) {
+  if (!this.isModified("name")) {
     next(); // skip it
     return; // stop this function from running
   }
   // this requires a real function
   this.slug = slug(`${this.school}_${this.code}`);
   // check if the slug is unique
-  const slugRegex = new RegExp(`^(${this.slug})((-[0-9]*$)?)`, 'i');
+  const slugRegex = new RegExp(`^(${this.slug})((-[0-9]*$)?)`, "i");
   const classWithSlug = await this.constructor.find({ slug: slugRegex });
   if (classWithSlug.length) {
     this.slug = `${this.slug}-${classWithSlug.length + 1}`;
@@ -38,4 +56,4 @@ classSchema.pre('save', async function (next) {
   // TODO: Make more resilient so slugs are unique
 });
 
-module.exports = mongoose.model('Class', classSchema);
+module.exports = mongoose.model("Class", classSchema);
