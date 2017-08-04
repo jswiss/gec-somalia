2cf73a55ddf2a67f9dc3b880471e6cf0fb4c3451
Joshua Swiss <swissjoshua@gmail.com>
2017-08-04 17:44:56 -0400

    form form up and working!

--- controllers/form.controller.js
+++ controllers/form.controller.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Form = mongoose.model('Form');
 
 exports.addForm = async (req, res) => {
-	req.body.store = req.params.id;
+	req.body.school = req.params.id;
 	const newForm = new Form(req.body);
 	await newForm.save();
 	req.flash('success', 'Form saved!');
